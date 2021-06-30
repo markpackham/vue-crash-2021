bf134a0f6f660a06f239427cc22d22299a34f727
mark <markpackham1@gmail.com>
2021-06-30 17:35:28 +0100

    toggle form

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button text="Add Task" color="green" />
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
   </header>
 </template>
 
@@ -15,6 +19,7 @@ export default {
       type: String,
       default: "Task Tracker",
     },
+    showAddTask: Boolean,
   },
   components: {
     Button,
