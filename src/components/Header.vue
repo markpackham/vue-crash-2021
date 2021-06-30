c5c4efdbd82c0eb5d977c190092629a39cdc5e2f
mark <markpackham1@gmail.com>
2021-06-30 12:14:46 +0100

    output tasks, https://youtu.be/qZXt1Aom3Cs?t=2470

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,24 @@
 <template>
   <header>
-    <h1>Task Tracker</h1>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
   </header>
 </template>
 
 <script>
+import Button from "./Button";
+
 export default {
   name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Task Tracker",
+    },
+  },
+  components: {
+    Button,
+  },
 };
 </script>
 
